8a52bb5786d178560986093ddf40d1ac863dde98
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-03-02 14:14:59 +0100

    Add Navbar with hover sub items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,38 +3,27 @@ import { Link } from "gatsby";
 
 import NavItem from "./nav-item";
 
-const NAV_ITEMS = [
-  {
-    label: "About",
-    to: "/about",
-  },
-  {
-    label: "Communities",
-    to: "/communities",
-  },
-  {
-    label: "Ecosystem",
-    to: "/ecosystem",
-  },
-  {
-    label: "Learn",
-    to: "/learn",
-  },
-  {
-    label: "News & Events",
-    to: "/news",
-  },
-];
+import Logo from "../images/logo.svg";
+import NavItemsJSON from "../content/nav-items.json";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row justify-between mx-auto container">
-      <Link to="/">Logo</Link>
-      <div className="flex flex-row gap-3">
-        {NAV_ITEMS.map((item) => (
-          <NavItem key={item.label} {...item} />
-        ))}
-        <div>How it works</div>
+    <div className="flex flex-col justify-end mx-auto container h-24 pb-4">
+      <div className="flex flex-row justify-between items-center">
+        <Link to="/">
+          <img src={Logo} />
+        </Link>
+        <div className="flex flex-row gap-6 items-center">
+          {NavItemsJSON.map((item) => (
+            <NavItem key={item.label} {...item} />
+          ))}
+          <div
+            className="bg-gray-100 px-8 py-4 cursor-pointer rounded-full hover:bg-gray-300"
+            onClick={() => console.log("TODO")}
+          >
+            How it works
+          </div>
+        </div>
       </div>
     </div>
   );
