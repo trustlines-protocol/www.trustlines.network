b5ab46558e1b057593ead585a51d957ad2162279
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-03-04 15:18:39 +0100

    Add gatsby-transformer-json plugin
    We can still switch to md files with frontmatter if wanted later.
    I just did not know how to use arrays/list there?
    So for now I am using JSON files as sources for the use case items

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-yaml`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-layout`,
       options: {
