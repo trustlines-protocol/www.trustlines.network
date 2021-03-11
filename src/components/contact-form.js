60f8d23ce47c61dd4739c65306e8cbdb5663eb81
Daniel Dimitrov <daniel@compojoom.com>
2021-03-11 23:05:34 +0100

    Submit contactus form to backend endpoint

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,26 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IconButton from "./icon-button";
 import ArrowRightIcon from "./icons/arrow-right";
 
 import GetInTouchTopicsJSON from "../content/get-in-touch-topics.json";
-
-// TODO: Source out
-const CONTACT_MAIL = "";
-const FORM_POST_URL = "";
+import CloseButton from "./close-button";
 
 export default function ContactForm(props) {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [errors, setErrors] = useState(false);
+  const CONTACT_MAIL = process.env.GATSBY_CONTACT_MAIL;
+  const FORM_POST_URL = process.env.GATSBY_FORM_POST_URL;
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(false);
+    setShowErrorMessage(false);
+    setErrors(false);
+
+    const data = new FormData(e.target);
+
+    const jsonData = Object.fromEntries(data.entries());
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: "POST",
+      mode: "cors",
+      headers: myHeaders,
+      body: JSON.stringify(jsonData),
+      redirect: "follow",
+    };
+
+    fetch(FORM_POST_URL, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) {
+          document.getElementById("contactUs").reset();
+          setShowSuccessMessage(true);
+        } else {
+          setErrors(result.errors);
+          setShowErrorMessage(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("something went wrong");
+      });
+  };
+
   return (
     <form
+      id={"contactUs"}
+      name={"contactUs"}
       className={`flex flex-col gap-4 mt-4 ${props.hidden ? "hidden" : ""}`}
       action={FORM_POST_URL}
       method="POST"
+      onSubmit={submitForm}
     >
+      {showErrorMessage && (
+        <div
+          className={"flex justify-between rounded p-2 bg-red-500 text-white"}
+        >
+          <div>
+            There was a problem submitting your message:
+            {Object.values(errors).map((error, i) => {
+              return <div key={"error" + i}>{error}</div>;
+            })}
+          </div>
+          <CloseButton
+            className="cursor-pointer hover:opacity-50"
+            onClick={() => {
+              setShowErrorMessage(false);
+            }}
+          />
+        </div>
+      )}
+
+      {showSuccessMessage && (
+        <div className={"flex justify-between rounded p-2 bg-majorelle-blue text-white"}>
+          Your message was submitted successfully. We'll get back to you ASAP.
+          <CloseButton
+            className="cursor-pointer hover:opacity-50"
+            onClick={() => {
+              setShowSuccessMessage(false);
+            }}
+          />
+        </div>
+      )}
+
       <div className="flex flex-col md:flex-row gap-4">
-        <Input placeholder="Name" required />
-        <Input placeholder="Email" required />
+        <Input placeholder="Name" name="name" required />
+        <Input placeholder="Email" name="email" required />
         <div className="flex-1 flex py-4 md:py-0 px-6 bg-gray-300 rounded-full overflow-hidden overflow-ellipsis">
           <select
+            name="topic"
             className="flex-1 bg-gray-300 text-sm font-semibold w-full"
             required
             defaultValue=""
@@ -38,6 +113,7 @@ export default function ContactForm(props) {
       </div>
       <div className="flex flex-col md:flex-row gap-4 items-center">
         <textarea
+          name="message"
           className="w-full md:w-auto flex-1 px-6 py-4 bg-gray-300 rounded-full text-sm font-semibold"
           placeholder="Message"
           required
