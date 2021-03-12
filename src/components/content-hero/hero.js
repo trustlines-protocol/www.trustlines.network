90e28f4dd09691307d8c2bb29f45410599c1ec4d
Daniel Dimitrov <daniel@compojoom.com>
2021-03-12 13:50:17 +0100

    Remove join button

--- src/components/content-hero/hero.js
+++ src/components/content-hero/hero.js
@@ -1,8 +1,6 @@
 import React from "react";
-import IconButton from "../icon-button";
-import CircleIcon from "../icons/circle";
 
-export default function ContentHero(props) {
+export default function ContentHero() {
   return (
     <div className={"container mx-auto px-2 mb-10 mt-6 lg:mb-20 lg:mt-12"}>
       <h1
@@ -17,15 +15,6 @@ export default function ContentHero(props) {
         The Trustlines Network is the first project to create a universally
         accessible medium of exchange.
       </p>
-      <IconButton
-        href={""}
-        label={"Join"}
-        textColor="white"
-        bgColor="black"
-        hoverBgColor="coral-red"
-        Icon={<CircleIcon className={"bg-coral-red group-hover:bg-white"} />}
-        glowColor="coral-red"
-      />
     </div>
   );
 }
